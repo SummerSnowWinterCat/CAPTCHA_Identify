4a8890b28997070a16b63f6e8be52f57df2e4ee8
WinterCat <summersnowwintercat@gmail.com>
2019-11-21 00:19:53 +0900

    update test image 0.03

--- test_module/test.py
+++ test_module/test.py
@@ -1,6 +1,10 @@
 import captcha_image_process.image_process as i_process
 from PIL import Image
 import numpy
+import os
 
 if __name__ == '__main__':
-    i_process.image_binarization(image_file_path='../test_image/test_image_box/4.png',save_path='../captcha_binarization_images/')
+    path = '../test_image/captcha_single_image(32x32)/'
+    for i in os.listdir(path):
+        i_process.image_binarization(path + i, save_path='../captcha_binarization_images/')
+    # i_process.image_binarization(image_file_path='../test_image/test_image_box/4.png',save_path='../captcha_binarization_images/')
