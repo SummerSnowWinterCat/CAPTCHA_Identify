bf3b71bc9d0b0a7557261e7b6ed23446ef688e17
WinterCat <summersnowwintercat@gmail.com>
2019-11-23 19:05:18 +0900

    update image_process 0.33

--- test_module/test_array.py
+++ test_module/test_array.py
@@ -4,6 +4,7 @@ import numpy
 import re
 import captcha_image_process.image_process as i_process
 import captcha_image_process.image_data_process as d_p
+import matplotlib.pyplot as plot
 
 # d_p.create_image_char(image_width=60, image_height=60, char=9, count=1000, background='white',
 #                      save_file_path='../captcha_train_images/9/')
@@ -20,7 +21,7 @@ def init_binarization_images():
     for train in range(len(train_data)):
         print(train_data[train])
         vector, label = i_process.create_binarization_image_vector(image_file_path=train_data[train],
-                                                                   save_file_path=binarization_path, block_size=6)
+                                                                   save_file_path=binarization_path, block_size=10)
         vector_data_path = i_process.save_vector_file(data=vector, file_name=str(label), save_file_path=vector_path)
         vector_data.append(vector_data_path)
         print('vector_create No.{}'.format(train))
@@ -28,5 +29,17 @@ def init_binarization_images():
 
 
 if __name__ == '__main__':
-    vector, label = i_process.vector_file_unfreeze(train_data_path=vector_path)
-    print(len(vector),len(label))
+    # init_binarization_images()
+    test_data, test_label = i_process.create_binarization_image_vector(image_file_path='../test_data/unknown.png',
+                                                                       block_size=10,
+                                                                       save_file_path='../test_image/test_image_box/')
+    # sample data
+    vector_data, vector_label = i_process.vector_file_unfreeze(train_data_path=vector_path)
+    if len(vector_data) == len(vector_label):
+        print('unfreeze complete')
+    for data in range(len(vector_data)):
+        result = (test_data - vector_data[data])
+        result = numpy.sum(result)
+        plot.scatter(x=result, y=vector_label[data], alpha=0.8, s=30)
+
+    plot.show()
