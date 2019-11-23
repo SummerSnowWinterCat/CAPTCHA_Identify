0a19f20e1e286a1a6b727658e87eaee3024c687f
WinterCat <summersnowwintercat@gmail.com>
2019-11-23 14:19:02 +0900

    update test image 0.21

--- captcha_image_process/k_nn_mod.py
+++ captcha_image_process/k_nn_mod.py
@@ -16,7 +16,7 @@ _block_size = 10
 if __name__ == '__main__':
     test_data, test_label = process.create_image_vector(image_file_path='../test_data/unknown.png',
                                                         block_size=_block_size,
-                                                        save_file_path='../captcha_binarization_images/')
+                                                        save_file_path='../test_image/test_image_box/')
     for path in os.listdir(_image_binarization_path):
         if re.search(r'\.(png|jpeg|jpg)$', path) is None:
             continue
@@ -24,10 +24,12 @@ if __name__ == '__main__':
             data, label = process.create_image_vector(image_file_path=_image_binarization_path + path,
                                                       block_size=_block_size,
                                                       save_file_path='../captcha_binarization_images/')
-            result = test_data - data
+            result = (test_data - data)
+            # 测试数据 与 样本数据的 距离
             result = numpy.sum(result)
+            print('test_data -  label:{} = {}'.format(label, result))
             plot.scatter(x=result, y=label, alpha=0.8, s=30)
 
-            # process.save_vector_file(data=data, file_name=label,save_file_path=_vector_file_path)
-    #plot.scatter(numpy.sum(test_data), y=test_label, alpha=0.8, s=40)
+    # process.save_vector_file(data=data, file_name=label,save_file_path=_vector_file_path)
+    # plot.scatter(numpy.sum(test_data), y=test_label, alpha=0.8, s=40)
     plot.show()
