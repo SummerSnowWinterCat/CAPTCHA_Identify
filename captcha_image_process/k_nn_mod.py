b76ad53e9c76686553126813ad96d1d945bab633
WinterCat <summersnowwintercat@gmail.com>
2019-11-25 19:23:39 +0900

    first version 0.39 upload run.py knn mod.py

--- captcha_image_process/k_nn_mod.py
+++ captcha_image_process/k_nn_mod.py
@@ -1,51 +1,80 @@
 import numpy
 import os
-import time
 import re
 import captcha_image_process.image_process as process
-import math
-import matplotlib.pyplot as plot
 
 
 def init_binarization_images(train_path, binarization_path, vector_path):
+    '''
+    This function is init train file and train data
+    :param train_path:train data file path
+    :param binarization_path:binarization file path
+    :param vector_path:train image data vector path
+    :return: 1 no message
+    '''
     vector_data = []
     train_data = process.get_training_data_path(train_data_path=train_path)
     for train in range(len(train_data)):
-        print(train_data[train])
         vector, label = process.create_binarization_image_vector(image_file_path=train_data[train],
                                                                  save_file_path=binarization_path, block_size=10)
         vector_data_path = process.save_vector_file(data=vector, file_name=str(label), save_file_path=vector_path)
         vector_data.append(vector_data_path)
-        print('vector_create No.{}'.format(train))
-    return 0
+        # print('vector_create No.{}'.format(train))
+    return 1
 
 
 def euclidean_distance(test_data, train_data):
+    '''
+    This function is euclidean distance match
+    :param test_data:test image data --vector
+    :param train_data:train image data --vector
+    :return:result --distance
+    '''
     _result = numpy.sqrt(numpy.sum(numpy.square(test_data - train_data)))
     return _result
 
 
 def load_test_vector(image_file_path, block_size, save_file_path):
+    '''
+    This function is load test data file vector
+    :param image_file_path:test image file path
+    :param block_size:resize vector 60*60->10*10 = 10
+    :param save_file_path:test image save file path
+    :return:test data and test label
+    '''
+    _image_file_path = []
+    _test_data = []
+    _test_label = []
     if len(os.listdir(image_file_path)) > 0:
-        for dir in os.listdir(image_file_path):
-            print(dir)
-            if re.search(r'\.(png|jpeg|jpg)$', dir) is None:
-                print('Error No test images')
-                return -1
+        for dirs in os.listdir(image_file_path):
+            full_path = image_file_path + dirs
+            if re.search(r'\.(png|jpeg|jpg)$', full_path) is None:
+                continue
             else:
-                print('ok')
+                _image_file_path.append(full_path)
+        if len(_image_file_path) > 0:
+            for images in _image_file_path:
+                test_data, test_label = process.create_binarization_image_vector(
+                    image_file_path=images,
+                    block_size=block_size,
+                    save_file_path=save_file_path)
+                _test_data.append(test_data)
+                _test_label.append(test_label)
+            return _test_data, _test_label
+        else:
+            return -1
     else:
         return -1
 
-    # test_data, test_label = process.create_binarization_image_vector(
-    #     image_file_path=image_file_path,
-    #    block_size=block_size,
-    #    save_file_path=save_file_path)
-
-    return  # test_data, test_label
-
 
 def data_check(data, data_label):
+    '''
+    This function is check length for data and label
+    same or not same
+    :param data:data file --list/array
+    :param data_label:label file --list/array
+    :return:boolean true or false
+    '''
     if len(data) == len(data_label):
         print('unfreeze complete')
         return True
@@ -54,4 +83,10 @@ def data_check(data, data_label):
 
 
 def k_search(k, result):
+    '''
+    This function is forecast range for k
+    :param k: k is range
+    :param result: result data
+    :return:forecast data --list/array
+    '''
     return sorted(result, key=lambda x: x[1])[k][0]
--- captcha_image_process/run.py
+++ captcha_image_process/run.py
@@ -1,35 +1,43 @@
 from captcha_image_process import image_data_process, image_process, k_nn_mod
 import os
 
-train_path = '../captcha_train_images/'
-binarization_path = '../captcha_binarization_images/'
-vector_path = '../train_data/'
-test_image_file_path = '../test_data/'
-test_image_save_path = '../test_image/test_image_box/'
-block_size = 10
-result = []
-k = 3
+train_path = '../captcha_train_images/'  # sample or train image (origin)
+binarization_path = '../captcha_binarization_images/'  # image binarization file path
+vector_path = '../train_data/'  # train data file path (vector file)
+test_image_file_path = '../test_data/'  # test data file path
+test_image_save_path = '../test_image/test_image_box/'  # test data binarization file path
+block_size = 10  # block size is resize image binarization like 60*60->10*10 = 10
+k = 3  # this is forecast range
+forecast_num = []  # forecast result
 if __name__ == '__main__':
     # step.01
     # init binarization image
+    print('<<<init>>>')
     k_nn_mod.init_binarization_images(train_path=train_path, binarization_path=binarization_path,
                                       vector_path=vector_path)
     # step.02
     # get test data
+    print('<<<load test data>>>')
     test_data, test_label = k_nn_mod.load_test_vector(image_file_path=test_image_file_path, block_size=block_size,
                                                       save_file_path=test_image_save_path)
     # step.03
     # unfreeze train data
+    print('<<<load train data>>>')
     vector_data, vector_label = image_process.vector_file_unfreeze(train_data_path=vector_path)
 
     # step.04 check data
     # get result
-    if k_nn_mod.data_check(data=vector_data, data_label=vector_label):
-        for data in range(len(vector_data)):
-            print('load..>>{}'.format(data))
-            result.append(
-                [vector_label[data], k_nn_mod.euclidean_distance(test_data=test_data, train_data=vector_data[data])])
-        for k in k_nn_mod.k_search(k, result):
-            print('This test data is :[ {} ]'.format(k))
+    if k_nn_mod.data_check(data=vector_data, data_label=vector_label) and k_nn_mod.data_check(data=test_data,
+                                                                                              data_label=test_label):
+
+        for test in range(len(test_data)):
+            result = []
+            for data in range(len(vector_data)):
+                print('load..>>{}'.format(data))
+                result.append(
+                    [vector_label[data],
+                     k_nn_mod.euclidean_distance(test_data=test_data[test], train_data=vector_data[data])])
+            forecast_num.append(k_nn_mod.k_search(k=k, result=result))
+        print('forecast:', forecast_num)
     else:
         print('error[!] ,Step.03')
