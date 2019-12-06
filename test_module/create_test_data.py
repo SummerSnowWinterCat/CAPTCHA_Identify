8da982ce7c39a30f38bdaaa03b6c66444398f580
WinterCat <38034385+SummerSnowWinterCat@users.noreply.github.com>
2019-12-06 23:00:51 +0900

    update train images

--- test_module/create_test_data.py
+++ test_module/create_test_data.py
@@ -4,7 +4,13 @@ import os
 
 def create_train_data():
     print("<Create Train Data />")
-    for i in range(0,10):
-        d_process.create_image_char(image_height=60,image_width=60,char=i, count=1000, background='white', save_file_path='../captcha_train_images/{}/'.format(i))
+    for i in range(0, 10):
+        d_process.create_image_char(image_height=60, image_width=60, char=i, count=1000, background='white',
+                                    save_file_path='../captcha_train_images/{}/'.format(i))
     return 1
 
+
+def create_train_data_once(number):
+    d_process.create_image_char(image_height=60, image_width=60, char=number, count=1, background='white',
+                                save_file_path='../captcha_train_images/{}/'.format(number))
+    return 1
